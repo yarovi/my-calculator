189104526654c08ae3c99c717b8f5c0e0a91ea06
Yasmani <tas185@gmail.com>
2021-09-29 01:55:34 -0500

    https://github.com/yarovi/Front-React-2021.git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React,{Component} from 'react';
+import FirstComponent from './components/FirstComponent'
+import SecondComponent from './components/SecondComponent/'
+import ThirdComponent from './components/ThirdComponent'
+
+import Counter from './components/counter/counter'
+
+class App extends Comment {
+  render (){
+    return (
+      <div className="App" >
+        <LearningComponents></LearningComponents>
+      </div>
+    );
+  }
+}
+
+class LearningComponents extends Component{
+  render (){
+    return (
+      <div className="LearningComponents"
+      >
+        My Hello World
+        <FirstComponent></FirstComponent>
+        <SecondComponent></SecondComponent>
+        <ThirdComponent></ThirdComponent>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ThirdComponent.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+//class Component
+class ThirdComponent extends Component {
+    render () {
+        return (
+            <div className="thirdComponent">
+                ThirdComponent
+            </div>
+        )
+    }
+}
+
+export default ThirdComponent
